af494353c61aae0142c72382ba5012a44b8e6315
Azat <8280770+azataiot@users.noreply.github.com>
2022-08-23 17:37:58 +0300

    update

--- lib/helpers/logging.dart
+++ lib/helpers/logging.dart
@@ -1,3 +1,9 @@
 import 'package:logger/logger.dart';
 
-final logger = Logger(printer: PrefixPrinter(PrettyPrinter(colors: false)));
+final logger = Logger(
+  // TODO: remove this in production
+  filter: null,
+  printer: PrefixPrinter(
+    PrettyPrinter(colors: false),
+  ),
+);
