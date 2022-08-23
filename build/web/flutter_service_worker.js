3376617399fd460a44fb0b80a5e03f391234bbdf
Azat <8280770+azataiot@users.noreply.github.com>
2022-08-23 17:30:38 +0300

    now building the app in profile mode

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "57ce47b1ce5e395526f5d048980b1798",
-"index.html": "1800798e74043be5f163b96429723a8d",
-"/": "1800798e74043be5f163b96429723a8d",
-"main.dart.js": "803b0d40ef276b8cb8761a9c011e9d55",
+"index.html": "fd1825902b3b1cebff03a98a8f8be78a",
+"/": "fd1825902b3b1cebff03a98a8f8be78a",
+"main.dart.js": "86eba3a8eb9c91a0ef48d60d9ff3f918",
 "flutter.js": "eb2682e33f25cd8f1fc59011497c35f8",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
